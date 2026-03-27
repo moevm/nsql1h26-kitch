ec603ff2104eb9b1914f0363af85073e00398a88
ShcherbakMaxim <maksim8305@mail.ru>
2026-03-27 20:06:35 +0300

    feat: implement MongoDB connection with test users

---
+++ backend/app/data/database.py
@@ -0,0 +1,17 @@
+
+import os
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+# uri = "mongodb+srv://<db_username>:<db_password>@cluster0.yevxekt.mongodb.net/?appName=Cluster0"
+uri = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Connected to MongoDB!")
+except Exception as e:
+    print(e)
