6ad905ae73f95d0a1394dc03647cebdd484ea637
ShcherbakMaxim <maksim8305@mail.ru>
2026-04-06 18:23:47 +0300

    feat: add Pydantic models for materials and designs

---
+++ backend/app/models/material.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from .base import MongoBase
+from datetime import datetime, timezone
+
+def utc_now() -> datetime:
+    return datetime.now(timezone.utc)
+
+
+class Material(BaseModel):
+    name: str
+    remain: int
+    coast: int
+
+class MaterialUpdate(BaseModel):
+    name: Optional[str] = None
+    remain: Optional[int] =  None
+    coast: Optional[int] = None
+
+class MaterialInDB(MongoBase):
+    name: str
+    remain: int
+    coast: int
+    updated_at: datetime = Field(default_factory=utc_now)
